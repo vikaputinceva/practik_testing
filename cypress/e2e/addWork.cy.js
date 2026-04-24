d4ea16f205c1d980f0c1e7a46a28854c0c26b3fe
vikaputinceva <vikaputin2008@gmail.com>
2026-04-25 00:44:10 +0700

    fix2

--- cypress/e2e/addWork.cy.js
+++ cypress/e2e/addWork.cy.js
@@ -1,37 +1,75 @@
-describe('Заявки на роль', () => {
-
-    const loginUrl = 'https://dev.profteam.su/login';
-
-    const login = 'puvev';
-    const password = 'Vikaputin_2008';
+describe('Тесты личного кабинета', () => {
+    const LOGIN_URL = 'https://dev.profteam.su/login';
+    const USERNAME = 'puvev';
+    const PASSWORD = 'Vikaputin_2008';
 
     beforeEach(() => {
-        cy.visit(loginUrl);
+    cy.visit(LOGIN_URL);
 
-        cy.get('[autocomplete="username"], input').first().type(login);
-        cy.get('[autocomplete="current-password"], input').type(password);
+    cy.get('[autocomplete="username"]').type(USERNAME);
+    cy.get('[autocomplete="current-password"]').type(PASSWORD);
+    cy.get('form').contains('button', ' Войти ').click();
 
-        cy.contains('Войти').click();
+    cy.intercept('/api/users/info').as('userInfo');
+    cy.wait('@userInfo');
+
+    cy.get('body').then(($body) => {
+        if ($body.find('.page-nav__role-block button').length) {
+            cy.get('.page-nav__role-block button').click();
+        }
+    });
+});
+
+    describe('Создание ЛК работодателя', () => {
+
+        it('Успешная заявка', () => {
+            cy.contains('p', 'Я являюсь представителем коммерческой организации').click();
+            cy.contains('p', 'Создание нового личного кабинета работодателя').click();
+
+            cy.get('[placeholder="Название вашей организации"]').type('New workersы');
+            cy.get('[placeholder="Адрес вашей организации"]').type('Уличка пушкина а дом калатушкаина');
+            cy.get('[placeholder="Описание вашей организации"]').type('Описание организации');
+
+            cy.get('.create-company-form__description-block button')
+                .should('be.visible')
+                .should('not.be.disabled')
+                .click();
+        });
+
+        it('Данные пустые', () => {
+            cy.contains('p', 'Я являюсь представителем коммерческой организации').click();
+            cy.contains('p', 'Создание нового личного кабинета работодателя').click();
+        });
     });
 
-    it('Подача заявки на работодателя', () => {
+    describe('Создание ЛК образовательной организации', () => {
 
-        cy.contains('Профиль').click();
-        cy.contains('Стать работодателем').click();
+        it('Успешная заявка', () => {
+            cy.contains('p', 'Я являюсь представителем образовательной организации').click();
+            cy.contains('p', 'Создание нового личного кабинета ОУ').click();
 
-        cy.contains('Отправить заявку').click();
+            cy.get('[placeholder="Название вашей организации"]').type('New workersы');
+            cy.get('[placeholder="Адрес вашей организации"]').type('Уличка пушкина а дом калатушкаина');
+            cy.get('[placeholder="Описание вашей организации"]').type('Описание организации');
 
-        cy.contains('Заявка отправлена').should('be.visible');
+            cy.get('.create-company-form__description-block button')
+                .should('be.visible')
+                .should('not.be.disabled')
+                .click();
+        });
+
+        it('Данные пустые', () => {
+            cy.contains('p', 'Я являюсь представителем образовательной организации').click();
+            cy.contains('p', 'Создание нового личного кабинета ОУ').click();
+        });
     });
 
-    it('Подача заявки на учебное заведение', () => {
+    describe('Добавление роли студента', () => {
 
-        cy.contains('Профиль').click();
-        cy.contains('Стать учебным заведением').click();
+        it('Успешный выбор роли', () => {
+            cy.contains('p', 'Я являюсь студентом').click();
+        });
 
-        cy.contains('Отправить заявку').click();
-
-        cy.contains('Заявка отправлена').should('be.visible');
     });
 
 });
